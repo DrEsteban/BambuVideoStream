5385fbd65951b466e26882b91ca310bb109ce9e4
Steven Ryland <stevenry@outlook.com>
2024-07-10 16:58:13 -0700

    Output update failures and add Directory.Build.targets to sln

--- BambuVideoStream/Program.cs
+++ BambuVideoStream/Program.cs
@@ -27,29 +27,40 @@ static async Task UpdateCheckAsync(string[] args)
     // check for new version
     if (mgr.IsInstalled)
     {
-        var newVersion = await mgr.CheckForUpdatesAsync();
-        if (newVersion != null)
+        Console.WriteLine("Checking for updates...");
+        try
         {
-            Console.WriteLine($"New update found! ({mgr.CurrentVersion} -> {newVersion.TargetFullRelease.Version})");
-            Console.WriteLine("Press 'y' within 10 seconds to update...");
-            var sw = Stopwatch.StartNew();
-            while (!Console.KeyAvailable && sw.Elapsed < TimeSpan.FromSeconds(10))
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion != null)
             {
-                await Task.Delay(250);
+                Console.WriteLine($"New update found! ({mgr.CurrentVersion} -> {newVersion.TargetFullRelease.Version})");
+                Console.WriteLine("Press 'y' within 10 seconds to update...");
+                var sw = Stopwatch.StartNew();
+                while (!Console.KeyAvailable && sw.Elapsed < TimeSpan.FromSeconds(10))
+                {
+                    await Task.Delay(250);
+                }
+                if (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Y)
+                {
+                    Console.WriteLine("Update skipped.");
+                    return;
+                }
+    
+                Console.WriteLine("Updating...");
+                // download new version
+                await mgr.DownloadUpdatesAsync(newVersion, progress => Console.WriteLine($"{progress}% completed", progress));
+                Console.WriteLine("Download completed. Restarting...");
+    
+                // install new version and restart app
+                mgr.ApplyUpdatesAndRestart(newVersion, args);
             }
-            if (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Y)
-            {
-                Console.WriteLine("Update skipped.");
-                return;
-            }
-
-            Console.WriteLine("Updating...");
-            // download new version
-            await mgr.DownloadUpdatesAsync(newVersion, progress => Console.WriteLine($"{progress}% completed", progress));
-            Console.WriteLine("Download completed. Restarting...");
-
-            // install new version and restart app
-            mgr.ApplyUpdatesAndRestart(newVersion, args);
+        }
+        catch (Exception e)
+        {
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine($"Error checking for updates: {e.GetType().Name}: {e.Message}");
+            Console.ForegroundColor = c;
         }
     }
 }
